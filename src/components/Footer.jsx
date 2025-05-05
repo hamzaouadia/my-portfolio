612d0291bfdb317e4e5df42d65eafe3c72856824
mak3r <mak3r@kali>
2025-05-05 05:30:18 +0100

    Adding the real urls to my linkedin and gmail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const paths = [
     { name: 'github', path: '/my-portfolio/assets/github-logo.svg', url: 'https://github.com/hamzaouadia' },
-    { name: 'gmail', path: '/my-portfolio/assets/gmail-logo.svg', url: 'mailto:your-email@gmail.com' },
-    { name: 'instagram', path: '/my-portfolio/assets/instagram-logo.svg', url: 'https://instagram.com/your-profile' },
-    { name: 'linkedin', path: '/my-portfolio/assets/linkedin-logo.svg', url: 'https://linkedin.com/in/your-profile' },
+    { name: 'gmail', path: '/my-portfolio/assets/gmail-logo.svg', url: 'mailto:ouadia.h.dev@gmail.com' },
+    // { name: 'instagram', path: '/my-portfolio/assets/instagram-logo.svg', url: 'https://instagram.com/your-profile' },
+    { name: 'linkedin', path: '/my-portfolio/assets/linkedin-logo.svg', url: 'https://www.linkedin.com/in/hamza-ouadia-528220274/' },
 ];
 
 const Footer = () => {
